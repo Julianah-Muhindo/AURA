09582fea1cd54e7f9a4ad53779eefa242e84491d
Muhindo Julianah Alinethu <jjulianahmuhindo@gmail.com>
2026-07-07 10:09:41 +0300

    Initialize AURA project and create welcome screen.

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to AURA, Version 0.0.1. 
---
+++ src/main.py
@@ -0,0 +1,23 @@
+# ================================
+# AURA =Adaptive Universal Resource Assistant
+# Version: 0.0.1
+# Author: Julianah Muhindo Alinethu 
+# ================================
+
+print("===============================")
+print("                     AURA")
+print("Adaptive Universal Resource Assistant")
+print("Version: 0.0.1") 
+print("================================") 
+print("")
+
+print("Initializing AURA...As you wait, fun fact, AURA started off as model to help the author learn python from scratch")
+
+print("") 
+
+print("Welcome!")
+
+print ("AURA is now running...Enjoy your experience!") 
+
+
+
